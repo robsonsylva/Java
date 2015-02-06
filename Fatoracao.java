f10f6f1ece4876238303ad9e5f1269b8b9cfb4ed
Robson S. Sousa <rob_sylva@yahoo.com.br>
2015-02-06 09:51:56 -0300

    Inicio: Entrada do numero 06/02/15

---
+++ Fatoracao.java
@@ -0,0 +1,18 @@
+/***********************************************************
+				Fatoracao Completa de um Numero
+				06/02/2015
+ ***********************************************************/
+import java.util.Scanner;
+public class Fatoracao {
+	public static void main (String[] args){
+		int numero = 0;
+		int divisor = 1;
+		boolean flag = false;
+		Scanner scan = new Scanner(System.in);
+		while (numero<=1){
+			System.out.println("Entre um numero inteiro maior ou igual a 2: ");
+			numero = scan.nextInt();
+		}
+
+	}
+}
